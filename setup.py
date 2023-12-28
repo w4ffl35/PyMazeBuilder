523ca39a502a46a92231f0de21dace19fab7663c
w4ffl35 <w4ffl35@ml1.net>
2023-12-28 14:58:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pymaze',
-    version="0.0.1",
+    name='pymazebuilder',
+    version="1.0.0",
     author="w4ffl35",
-    description="Python maze generator",
+    description="Python Maze Builder: generate perfect mazes with a growing tree algorithm. Generate rooms, multiple floors and more.",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     keywords="maze, generator, procedural, dungeon, game, roguelike, procedural generation, procedural dungeon, procedural maze, procedural game, procedural roguelike, procedural dungeon generator, procedural maze generator, procedural game generator, procedural roguelike generator",
     license="AGPL-3.0",
     author_email="",
-    url="https://github.com/w4ffl35/pymaze",
+    url="https://github.com/w4ffl35/pymazebuilder",
     package_dir={"": "src"},
     packages=find_packages("src"),
     python_requires=">=3.10.0",
