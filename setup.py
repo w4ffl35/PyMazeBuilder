50b299ac8381d706eb23c61d88f0aca3ca29ce40
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-03-14 21:20:53 -0600

    add missing properties to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='pymazebuilder',
     version='1.1.0',
+    author="w4ffl35",
+    description="RPG",
+    long_description=open(
+        "README.md",
+        "r",
+        encoding="utf-8"
+    ).read(),
+    long_description_content_type="text/markdown",
+    keywords="maze, builder, generator, rpg, game, development",
+    license="AGPL-3.0",
+    author_email="contact@capsizegames.com",
+    url="https://github.com/w4ffl35/PyMazeBuilder",
     packages=find_packages("src"),
     package_dir={"": "src"},
     entry_points={
