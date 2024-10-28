1f09e2097c3d4141095092e4388b3ce8d89e1f15
w4ffl35 <w4ffl35@ml1.net>
2024-10-28 08:23:33 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymazebuilder',
-    version='1.3.0',
+    version='1.3.1',
     author="w4ffl35",
     description="RPG",
     long_description=open(
